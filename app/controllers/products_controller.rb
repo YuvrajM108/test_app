6a23ea2d59633ecdafb5fcf87c12d305a1d592c8
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-06-30 17:05:54 -0400

    Add controller methods and test new form on server

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,19 @@
 class ProductsController < ApplicationController
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to new_product_path
+    else
+      render :new
+    end 
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price)
   end
 end
